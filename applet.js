d90ed4b48236efd37a12232d064727da207f2739
NikoKrause <nikokrause@gmx.de>
2016-05-20 15:30:53 +0200

    menu applet: fix favorites and system buttons receiving incorrect styling and appearing non-reactive.

--- applet.js
+++ applet.js
@@ -2048,9 +2048,9 @@ MyApplet.prototype = {
                 parent._vis_iter.reloadVisible();
             }
             let _maybePreviousActor = this._activeActor;
-            if (_maybePreviousActor && this._activeContainer === this.applicationsBox) {
+            if (_maybePreviousActor && this._activeContainer !== this.categoriesBox) {
                 this._previousSelectedActor = _maybePreviousActor;
-                this._clearPrevAppSelection();
+                this._clearPrevSelection();
             }
             if (parent === this.categoriesBox && !this.searchActive) {
                 this._previousSelectedActor = _maybePreviousActor;
@@ -2065,9 +2065,13 @@ MyApplet.prototype = {
         button.actor.connect('enter-event', _callback);
     },
 
-    _clearPrevAppSelection: function(actor) {
+    _clearPrevSelection: function(actor) {
         if (this._previousSelectedActor && this._previousSelectedActor != actor) {
-            this._previousSelectedActor.style_class = "menu-application-button";
+	    if (this._previousSelectedActor._delegate instanceof ApplicationButton)
+                this._previousSelectedActor.style_class = "menu-application-button";
+            else if (this._previousSelectedActor._delegate instanceof FavoritesButton ||
+                     this._previousSelectedActor._delegate instanceof SystemButton)
+                this._previousSelectedActor.remove_style_pseudo_class("hover");
         }
     },
 
@@ -2237,7 +2241,7 @@ MyApplet.prototype = {
                 let place = places[i];
                 let button = new PlaceButton(this, place, place.name, this.showApplicationIcons);
                 this._addEnterEvent(button, Lang.bind(this, function() {
-                        this._clearPrevAppSelection(button.actor);
+                        this._clearPrevSelection(button.actor);
                         button.actor.style_class = "menu-application-button-selected";
                         this._scrollToButton(button);
                         this.selectedAppDescription.set_text(button.place.id.slice(16));
@@ -2290,7 +2294,7 @@ MyApplet.prototype = {
             for (let id = 0; id < MAX_RECENT_FILES && id < this.RecentManager._infosByTimestamp.length; id++) {
                 let button = new RecentButton(this, this.RecentManager._infosByTimestamp[id], this.showApplicationIcons);
                 this._addEnterEvent(button, Lang.bind(this, function() {
-                        this._clearPrevAppSelection(button.actor);
+                        this._clearPrevSelection(button.actor);
                         button.actor.style_class = "menu-application-button-selected";
                         this._scrollToButton(button);
                         this.selectedAppDescription.set_text(button.file.uri.slice(7));
@@ -2307,7 +2311,7 @@ MyApplet.prototype = {
             if (this.RecentManager._infosByTimestamp.length > 0) {
                 let button = new RecentClearButton(this);
                 this._addEnterEvent(button, Lang.bind(this, function() {
-                        this._clearPrevAppSelection(button.actor);
+                        this._clearPrevSelection(button.actor);
                         button.actor.style_class = "menu-application-button-selected";
                         this._scrollToButton(button);
                         }));
@@ -2446,7 +2450,7 @@ MyApplet.prototype = {
             this.selectedAppDescription.set_text(applicationButton.app.get_description());
         else
             this.selectedAppDescription.set_text("");
-        this._clearPrevAppSelection(applicationButton.actor);
+        this._clearPrevSelection(applicationButton.actor);
         applicationButton.actor.style_class = "menu-application-button-selected";
         this._scrollToButton(applicationButton);
     },
